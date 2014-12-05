405eaea69d765e41ad299de1080aa5fc5256e4ab
pepe81 <jose.81@hotmail.com>
2014-12-05 12:28:37 +0100

    Añadidos cambios de hoy

--- JMySql/src/serpis/ad/Menu.java
+++ JMySql/src/serpis/ad/Menu.java
@@ -1,5 +1,58 @@
 package serpis.ad;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.*;
 
-public class Menu {
+public class Menu 
+{
+	public static void main(String[]args) throws ClassNotFoundException, SQLException
+	{
+		Scanner tcl = new Scanner(System.in);
+		int select;
+		String id;
+		String nombre;
+		long categoria;
+		BigDecimal precio;
+		Metodos metodos = null;
+		
+		do
+		{
+			System.out.println("***Menú Artículos***");
+			System.out.println("0. Salir");
+			System.out.println("1. Nuevo");
+			System.out.println("2. Editar");
+			System.out.println("3. Eliminar");
+			System.out.println("4. Listar");
+			select = tcl.nextInt();
+			tcl.nextLine();
+			
+			switch(select)
+			{
+				case 1:
+					System.out.println("Introduce el nombre del artículo: ");
+					nombre = tcl.nextLine();
+					System.out.println("Introduce la categoría del artículo: ");
+					categoria = tcl.nextLong();
+					System.out.println("Introduce el precio del artículo: ");
+					precio = tcl.nextBigDecimal();
+					
+					metodos.añadirArticulo(nombre, categoria, precio);
+					break;
+				case 2:
+					System.out.println("Introduce el id del artículo: ");
+					break;
+				case 3:
+					System.out.println("Introduce el id del artículo: ");
+					break;
+				case 4:
+					System.out.println("Introduce el id del artículo: ");
+					break;
+				case 0:
+					System.out.println("Saliendo del sistema");
+					break;
+			}
+		}
+		while(select!=0);
+	}
 
 }
--- JMySql/src/serpis/ad/Metodos.java
+++ JMySql/src/serpis/ad/Metodos.java
@@ -1,5 +1,39 @@
 package serpis.ad;
 
-public class Metodos {
+import java.math.BigDecimal;
+import java.sql.*;
+import java.text.DecimalFormat;
 
+public class Metodos 
+{
+	Conexion conexion;
+	static ResultSet rs;
+	
+	public Metodos()throws ClassNotFoundException,SQLException
+	{
+		conexion = new Conexion();
+	}
+	public boolean añadirArticulo(String nombre,Long categoria,BigDecimal precio)throws SQLException, ClassNotFoundException
+	{
+		String sql = "insert into articulo(nombre,categoria,precio)values("+"'"+nombre+"','"+categoria+"','"+precio+"')";
+		return conexion.insertar(sql);
+	}
+	public void listarArticulos() throws SQLException, ClassNotFoundException
+	{
+		String sql = "select * form articulo order by 1";
+		rs = conexion.select(sql);
+		while (rs.next())
+		{
+			System.out.printf("id=%4s nombre=%s categoria=%s precio=%s \n",
+					rs.getObject("id"),
+					rs.getObject("nombre"),
+					rs.getObject("categoria"),
+					rs.getObject("precio"));
+		}
+		rs.close();
+	}
+	public void borrarArticulo(String id) throws SQLException, ClassNotFoundException
+	{
+		
+	}
 }
