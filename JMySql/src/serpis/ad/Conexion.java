a4b5e88f394546c849636281f7409f259c96244c
pepe81 <jose.81@hotmail.com>
2014-12-05 10:26:46 +0100

    Modificaciones

--- JMySql/src/serpis/ad/Conexion.java
+++ JMySql/src/serpis/ad/Conexion.java
@@ -2,22 +2,30 @@ package serpis.ad;
 
 import java.sql.*;
 
-public class MySql 
+public class Conexion 
 {
 	private static Connection connection;
 	private static Statement stmt;
 	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException 
+	Conexion() throws ClassNotFoundException, SQLException 
 	{
 		//Class.forName("com.mysql.jdbc.Driver"); necesario en versiones del driver tipo 3 o anterior
-		System.out.println("Hola");
 		String url = "jdbc:mysql://localhost/dbprueba?";
 		String user = "root";
 		String password = "sistemas";
 		
 		connection = DriverManager.getConnection(url, user, password);
 		stmt = connection.createStatement();
-
+		
+//		PreparedStatement preparedStatement = connection.prepareStatement("select * from categoria where id = ?");
+//		preparedStatement.setLong(0, 7);
+//		ResultSet rs = preparedStatement.executeQuery();
+//		while(rs.next())
+//			System.out.printf("id=%4s nombre=%s\n", rs.getObject("id"),rs.getObject("nombre"));
+//		
+//		rs.close();
+//		preparedStatement.close();
+//		connection.close();
 	}
 	public boolean insertar(String sql) throws SQLException, ClassNotFoundException
 	{
